f4d10b6fe0bf06f5d2d8cf2e6c291e8f246f2af2
DavidKanma <david.kanma@gmail.com>
2026-08-23 19:23:26 -0700

    Add SQL query for ESG risk quintile ranking

--- sql/02_risk_quintile_ranking.sql
+++ sql/02_risk_quintile_ranking.sql
@@ -1,1 +1,8 @@
-
+q2 = """
+SELECT *,
+  NTILE(5) OVER (ORDER BY ESG_Risk_Score) AS ESG_Risk_Quintile
+FROM joined;
+"""
+quintiles = pd.read_sql(q2, conn)
+quintiles.to_csv("esg_returns_joined.csv", index=False)
+quintiles.groupby("ESG_Risk_Quintile")["YTD_Return_Pct"].mean().round(2)
